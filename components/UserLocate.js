46f78e286853c7205b0a703fe9b376d05dce8a6f
Sebastian Fechtrup <sebastian@wahnsinn.design>
2022-11-16 13:41:18 +0100

    edit user location

--- components/UserLocate.js
+++ components/UserLocate.js
@@ -2,21 +2,26 @@ import { useMap } from "react-leaflet";
 import Locate from "leaflet.locatecontrol";
 import "leaflet.locatecontrol/dist/L.Control.Locate.min.css";
 import * as L from "leaflet";
-
-
+import { useEffect } from "react";
 
 const UserLocate = () => {
   const map = useMap();
 
-  var lc = L.control
-    .locate({
-      position: "topleft",
-      flyTo: true,
-      strings: {
-        title: "Show me where I am, yo!",
-      },
-    })
-    .addTo(map);
+  useEffect(() => {
+    var lc = L.control
+      .locate({
+        position: "topleft",
+        flyTo: true,
+        strings: {
+          title: "Show me where I am, yo!",
+        },
+      })
+      .addTo(map);
+    return () => {
+      lc.remove();
+    };
+  }, []);
+
   return null;
 };
 
